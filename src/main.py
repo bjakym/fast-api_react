28e58e40f73bd2d0f8aef74070c09f03737f419d
Bohdan Jakym <dania.iakym@gmail.com>
2025-11-14 19:04:55 +0100

    Add CORS middleware to allow requests from specified origins

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from .router import router as currency_router
 from .init import cmc_client
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -17,7 +18,20 @@ app = FastAPI(lifespan=lifespan)
 
 app.include_router(currency_router)
 
+origins = [
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 if __name__ == "__main__":
     uvicorn.run(
-        "src.main:app", host="127.0.0.1", port=8000, log_level="info", reload=True
+        "src.main:app", host="0.0.0.0", port=8000, log_level="info", reload=True
     )
